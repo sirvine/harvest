97031f641d82b938cf1f53c030356c7a481b74d0
Sol Irvine <sol@densevector.com>
2012-06-11 16:19:38 -0400

    trying some type conversions to explain array subtraction fail

--- lib/harvest/plugins/hash_ext.rb
+++ lib/harvest/plugins/hash_ext.rb
@@ -1,8 +1,9 @@
 class Hash
   def assert_valid_keys(*valid_keys)
-    original_keys = keys.map{|key| key}
-    nonvalid_keys = (original_keys - valid_keys)
-    raise ArgumentError, "Invalid key(s): #{nonvalid_keys.join(", ")} | #{original_keys - valid_keys} / Passed key(s): #{original_keys.join(", ")} / Valid key(s): #{valid_keys.join(", ")}" unless nonvalid_keys.empty?
+    original_keys = keys.map{|key| key.to_s}
+    passed_keys = valid_keys.map{|key| key.to_s}
+    nonvalid_keys = (original_keys - passed_keys)
+    raise ArgumentError, "Invalid key(s): #{nonvalid_keys.join(", ")} | #{original_keys - valid_keys} / Passed key(s): #{original_keys.join(", ")} / Valid key(s): #{passed_keys.join(", ")}" unless nonvalid_keys.empty?
   end
 
   def assert_required_keys(*required_keys)

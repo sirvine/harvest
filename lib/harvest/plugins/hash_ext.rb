e076a36fcde452ed76003ba8c4ddcb109dd0d92d
Sol Irvine <sol@densevector.com>
2012-06-11 15:57:00 -0400

    working on passed keys

--- lib/harvest/plugins/hash_ext.rb
+++ lib/harvest/plugins/hash_ext.rb
@@ -2,7 +2,7 @@ class Hash
   def assert_valid_keys(*valid_keys)
     original_keys = keys.map{|key| key}
     invalid_keys = original_keys - valid_keys
-    raise ArgumentError, "Invalid key(s): #{invalid_keys.join(", ")} / Passed key(s): #{original_keys.join(", ")} / Valid key(s): #{valid_keys.join(", ")}" unless invalid_keys.empty?
+    raise ArgumentError, "Invalid key(s): #{invalid_keys.join(", ")} | #{invalid_keys.class.name} / Passed key(s): #{original_keys.join(", ")} | #{original_keys.class.name} / Valid key(s): #{valid_keys.join(", ")} | #{valid_keys.class.name}" unless invalid_keys.empty?
   end
 
   def assert_required_keys(*required_keys)
